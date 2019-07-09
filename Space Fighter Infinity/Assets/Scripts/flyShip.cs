5ca407ec3ad0315d56c1b52de0190bd1d60708d3
TheGameMaker <clarksmiley@comcast.net>
2019-07-09 19:01:09 -0400

    Sample template
    a template for the game

---
+++ Space Fighter Infinity/Assets/Scripts/flyShip.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flyShip : MonoBehaviour {
+
+    public float speed = 10f;
+    public float movementSpeed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(1* speed, 0,  Input.GetAxis("Horizontal")*Time.deltaTime*movementSpeed);
+	}
+}
