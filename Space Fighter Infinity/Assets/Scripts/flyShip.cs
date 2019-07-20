9a4fde75586ad487d17da85564f81b988946b903
TheGameMaker <clarksmiley@comcast.net>
2019-07-20 16:09:07 -0400

    Updated flight controls + working on spawning asteroids
    little buggy for now

--- Space Fighter Infinity/Assets/Scripts/flyShip.cs
+++ Space Fighter Infinity/Assets/Scripts/flyShip.cs
@@ -2,17 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class flyShip : MonoBehaviour {
+public class flyShip : MonoBehaviour
+{
 
-    public float speed = 10f;
-    public float movementSpeed;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        transform.Translate(1* speed, 0,  Input.GetAxis("Horizontal")*Time.deltaTime*movementSpeed);
-	}
+    void Start()
+    {
+        Debug.Log("fly ship script added to: " + gameObject.name);
+    }
+
+    void Update()
+    {
+        transform.position += transform.forward * Time.deltaTime * 50.0f;
+        transform.Rotate(Input.GetAxis("Horizontal"), 0.0f, -Input.GetAxis("Vertical"));
+    }
 }
